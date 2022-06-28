60dfab64e2c1278ea274aba015d9c397bdba72b0
Arnav Jhingran <ajhingran@wisc.edu>
2022-06-28 16:03:09 -0500

    adding file operations

---
+++ fileoperations.c
@@ -0,0 +1,27 @@
+//
+// Created by arniejhingran on 6/28/22.
+//
+#include "stdio.h"
+#include "string.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void setupTestDirectory(){
+    mkdir("testFiles", 0777);
+}
+void createFile(unsigned num){
+    char filename[32];
+    sprintf(filename, "testFiles/testfile%i.txt", num);
+    FILE* fileToWrite = NULL;
+    fileToWrite = fopen(filename, "w+");
+    if(fileToWrite != NULL){
+        fprintf(fileToWrite, "testfilegenerated");
+    }
+}
+
+int main(){
+    setupTestDirectory();
+    createFile(16);
+    setupTestDirectory();
+}
