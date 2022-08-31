37b3987b533cbb70682087656ea8704d8a804923
Arnav Jhingran <ajhingran@wisc.edu>
2022-08-31 00:35:49 -0500

    made graphs into scatter plot, fixed image size and formatting

--- metrics-scraper.py
+++ metrics-scraper.py
@@ -1,5 +1,6 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 current_dir = os.getcwd()
 metrics = os.path.join(current_dir, 'metrics')
@@ -14,13 +15,20 @@ for filename in os.listdir(metrics):
     if ".txt" in filename:
         with open(os.path.join(metrics, filename), 'r') as metric_file:
             data = metric_file.readlines()
+            if "read" in filename:
+                data = np.asarray(data, type=float)
+                for i in range(0, len(data)):
+                    if data[i] > 600000000:
+                        data.remove(data[i])
             total_entries = len(data)
             x = list(range(0, total_entries))
             y = data
             name = filename[:len(filename)-4]
-            plt.title(name)
-            plt.plot(x, y)
-            plt.savefig('metrics/'+name+'png')
+            plt.figure(figsize=(total_entries/100, 100), dpi=50)
+
+            plt.title(name, fontsize='108')
+            plt.scatter(x, y)
+            plt.savefig('metrics/'+name+'.png')
             #plt.show()
 
 
