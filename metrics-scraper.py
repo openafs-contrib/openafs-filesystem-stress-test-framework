44f8479485cf098911ddf67766df59e8b48bd620
Arnav Jhingran <ajhingran@wisc.edu>
2022-09-11 21:33:55 -0500

    added labels and pdf versions

--- metrics-scraper.py
+++ metrics-scraper.py
@@ -6,16 +6,19 @@ import numpy.ma
 
 
 #Plots given data from a file into a scatterplot
-def plot(data_param, filename_param):
+def plot(data_param, filename_param, ylabel):
     total_entries = len(data_param)
     x = list(range(0, total_entries))
     y = data_param
     s = [500 for i in range(total_entries)]
     name = filename_param[:len(filename_param) - 4]
     plt.figure(figsize=(300, 100), dpi=50)
-    plt.title(name, fontsize='108')
-    plt.scatter(x, y, s=s)
+    plt.xlabel("Duration of the Test", fontsize=256)
+    plt.ylabel(ylabel, fontsize=256)
+    plt.title(name, fontsize=512)
+    plt.scatter(x, y, marker='+', s=s)
     plt.savefig('metrics/' + name + '.png')
+    plt.savefig('metrics/' + name + '.pdf')
 
 
 #Reads through the metrics branch and plots visualization for all the gathered data
@@ -39,10 +42,10 @@ def main():
                     data_uncached = numpy.ma.masked_array(data, mask=data > 300000000)
                     data_cached = numpy.ma.masked_array(data, mask=data <= 300000000)
 
-                    plot(data_cached, 'cached_' + filename)
-                    plot(data_uncached, 'uncached_' + filename)
+                    plot(data_cached, 'cached_' + filename, "bytes/second")
+                    plot(data_uncached, 'uncached_' + filename, "bytes/second")
                 else:
-                    plot(data, filename)
+                    plot(data, filename, "time take for operation")
                 # plt.show()
 
 
